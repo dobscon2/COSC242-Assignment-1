a91f3d46a36baef7fa5db3d262ebe5a0eff126f3
Connor Dobson <dobco402@student.otago.ac.nz>
2021-08-04 14:52:37 +1200

    Created structure for asgn1.c file Created the basic skeleton for the main asgn1.c file

--- asgn1.c
+++ asgn1.c
@@ -1,1 +1,33 @@
-/* asgn1.c file */
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include "htable.h"
+#include "mylib.h"
+
+static void print_info(int freq, char *word) {
+    printf("-4d %s\n", freq, word);
+}
+
+int main(int argc, char* argv[]) {
+    const char *optstring = "d:e:p:s:t:h";
+    char option;
+
+    while ((option = getopt(argc, argv, optstring)) != EOF) {
+        switch (option) {
+            case 'd':
+                /* double hashing option */
+            case 'e':
+                /* display contents of hash table */
+            case 'p':
+                /* print stats */
+            case 's':
+                /* snapshot of table */
+            case 't':
+                /* tablesize */
+            case 'h':
+                /* display help message */
+            default:
+                /* display default message */
+        }
+    }
+}
